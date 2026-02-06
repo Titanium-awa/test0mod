35d50c61ed8564a2daefb007e8ba27768c33b126
Titanium-awa <Titanium-awa@outlook.com>
2026-02-06 17:46:32 +0800

    Initial commit

---
+++ src/client/java/titanium_awa/test0/Test0Client.java
@@ -0,0 +1,10 @@
+package titanium_awa.test0;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Test0Client implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
