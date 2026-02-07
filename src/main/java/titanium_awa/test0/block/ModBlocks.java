f92257191e7a374c3c1a7c3dcc31b57ced06afc0
Titanium-awa <196439259+Titanium-awa@users.noreply.github.com>
2026-02-07 13:04:35 +0800

    awa

--- src/main/java/titanium_awa/test0/block/ModBlocks.java
+++ src/main/java/titanium_awa/test0/block/ModBlocks.java
@@ -9,28 +9,10 @@ import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 import titanium_awa.test0.Test0;
 
-// public class ModBlocks1 {
-//    public static final Block TEST_BLOCK = new Block(AbstractBlock.Settings.create().strength(2.0f, 2.0f));
-//    public static final Block TEST_ORE = new Block(AbstractBlock.Settings.create().strength(1.0f, 1.0f));
-//
-//    public static void registerModBlocks() {
-//        Test0.LOGGER.info("Registering ModBlocks for " + Test0.MOD_ID);
-//
-//        // 先注册方块
-//        Registry.register(Registries.BLOCK, Identifier.of(Test0.MOD_ID, "test_block"), TEST_BLOCK);
-//        Registry.register(Registries.BLOCK, Identifier.of(Test0.MOD_ID, "test_ore"), TEST_ORE);
-//
-//        // 然后注册对应的物品
-//        Registry.register(Registries.ITEM, Identifier.of(Test0.MOD_ID, "test_block"),
-//                new BlockItem(TEST_BLOCK, new Item.Settings()));
-//        Registry.register(Registries.ITEM, Identifier.of(Test0.MOD_ID, "test_ore"),
-//                new BlockItem(TEST_ORE, new Item.Settings()));
-//    }
-//}
 
 public class ModBlocks {
-    public static final Block TEST_BLOCK = new Block(AbstractBlock.Settings.create().strength(2.0f, 2.0f));
-    public static final Block TEST_ORE = new Block(AbstractBlock.Settings.create().strength(1.0f, 1.0f));
+    public static final Block TEST_BLOCK = new Block(AbstractBlock.Settings.create().strength(2.5f, 2.5f));
+    public static final Block TEST_ORE = new Block(AbstractBlock.Settings.create().requiresTool().strength(3.0f, 3.0f));
 
     public static void registerModBlocks() {
         Test0.LOGGER.info("Registering ModBlocks for " + Test0.MOD_ID);
